04aee500b479465f46e06336548c6cb3c144d347
Fernanda Guimarães Santana <fernandaguimaraes0202@gmail.com>
2024-05-15 16:43:17 -0300

    desenho do jogo da velha pronto

--- logica-de-programacao/prof-sergio/matriz/script.js
+++ logica-de-programacao/prof-sergio/matriz/script.js
@@ -1,6 +1,7 @@
-let bt;
-let brokenLine;
-let tabuleiro = new Array(3);
+let bt,
+  brokenLine,
+  jogada = 1,
+  tabuleiro = new Array(3);
 
 for (let i = 0; i < tabuleiro.length; i++) {
   tabuleiro[i] = new Array(3);
@@ -14,8 +15,22 @@ for (let i = 0; i < tabuleiro.length; i++) {
     bt.setAttribute("type", "button");
     bt.setAttribute("id", "bt" + i + "" + j);
     bt.setAttribute("class", "bt-game" + i);
-    bt.setAttribute("onclick", "");
+    bt.setAttribute("onclick", "marca(" + i + "," + j + ")");
     bt.append(document.createTextNode(""));
     document.body.append(bt);
   }
+}
+
+function marca(linha, coluna) {
+  marcarCasa("bt" + linha + "" + coluna);
+}
+
+function marcarCasa(buttonName) {
+  if (jogada % 2 == 0) {
+    document.getElementById(buttonName).innerHTML = "O";
+  } else {
+    document.getElementById(buttonName).innerHTML = "X";
+  }
+  document.getElementById(buttonName).disabled = true;
+  jogada++;
 }
