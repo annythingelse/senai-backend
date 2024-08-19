a4be60004098a91b1a383e0cd46ba59c63864b07
Fernanda Guimarães Santana <fernandaguimaraes0202@gmail.com>
2024-08-19 17:11:20 -0300

    add: update and deleye

--- backend/ramon/controlador.js
+++ backend/ramon/controlador.js
@@ -41,14 +41,35 @@ const updateProduto = async (req,res) => {
         res.status(404).send({error:'not found'})
     }
     // atualizar o produto
+    for(const dado in dados) {
+       if(!dado in produto) {
+        console.log('erro esse dado não existe');
+        continue
+       }
+       produto[dado] = dados[dado];
+       fs.writeFile('./db.json', JSON.stringify(db), (err) => {
+           if (err){
+               res.status(500).send({error:'erro no servidor'})
+           }
+       })
+       res.status(200).send({produto})
+    }
 }
 const deleteProduto = async (req,res) => {
     const _id = req.params.id
     const lista_produtos = db.produtos
     const produto = lista_produtos.find(
         (produto) => produto.id == _id
-        )
+    )
     // deletar o produto
+    var idx = lista_produtos.indexOf(produto)
+    lista_produtos.splice(idx,1)
+    fs.writeFile('./db.json', JSON.stringify(db), (err) => {
+        if(err) {
+            res.status(500).send({error:'erro no servidor'})
+        }
+    })
+    res.status(204).send();
 }
 
 module.exports = {listProdutos, getProduto, createProduto, updateProduto, deleteProduto}
